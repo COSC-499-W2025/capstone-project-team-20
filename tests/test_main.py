44ac0c2e5f0480193f15fca5e4dfd1c3bb12ac57
branden kennedy <144386258+branden6@users.noreply.github.com>
2025-10-12 21:13:23 -0700

    Delete tests/test_main.py

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from src.main import main
-
-def test_main_runs():
-    try:
-        main()
-        assert True
-    except Exception as e:
-        assert False, f"main() raised an exception: {e}"
