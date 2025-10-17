4d2fcdc5099dadc5daeb0869b0c156dd584950dd
branden kennedy <brandenk88@gmail.com>
2025-10-17 15:27:42 -0700

    Added ConsentManager class. Consent Manager has 4 methods, init to initialize, has_user_consented to check if user has previously consented, request_consent to request consent if user has not given consent previously, and require_consent which checks if the user has consented, if they havent it will continuously send has_user_consented. Also added a few lines to main which calls require_consent. All simple for now, but functional and works as it should.

---
+++ src/ConsentManager.py
@@ -0,0 +1,33 @@
+from src.ConfigManager import ConfigManager
+
+class ConsentManager:
+
+    def __init__(self, db_path="config.db"):
+        self.manager = ConfigManager(db_path=db_path)
+
+    def has_user_consented(self):
+        #check if user has previously consented to allowing program to run
+        has_consented = self.manager.get("user_consent") 
+        return has_consented is True
+
+    def request_consent(self):
+        print("Do you give consent to scan your files?")
+        answer = input("(yes/no)")
+        answer.strip().lower() 
+        #allows yes, y and strips leading/trailing characters from String entered
+        consent = answer in ("yes", "y")
+
+        self.manager.set("user_consent", consent)
+        if consent:
+            print("Consent recorded in ConsentManager. Thankyou!")
+        else:
+            print("Consent recorded in ConsentManager. Program aborted.")
+        return consent
+    
+    def require_consent(self):
+        if not self.has_user_consented():
+            return self.request_consent()
+        return True
+
+
+    
