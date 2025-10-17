edc49606e553102b2b2287c029ec7184cc5cc29a
branden kennedy <brandenk88@gmail.com>
2025-10-17 15:27:42 -0700

    Added ConsentManager class. Consent Manager has 4 methods, init to initialize, has_user_consented to check if user has previously consented, request_consent to request consent if user has not given consent previously, and require_consent which checks if the user has consented, if they havent it will continuously send has_user_consented. Also added a few lines to main which calls require_consent. All simple for now, but functional and works as it should.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from src.ConsentManager import ConsentManager
+
 def main():
-    print("Team 20 is surely the best team!")
+    consent = ConsentManager()
+
+    # Uncomment two lines below to reset consent for testing
+    # from src.ConfigManager import ConfigManager
+    # ConfigManager().delete("user_consent")
+
+    if not consent.require_consent():
+        print("Consent not given. Exiting program.")
+        return
+    # if answer is yes, consent persists
+    # if answer is no, will re ask for consent each time
+    print("Consent confirmed in main.")
+
+    
 
 if __name__ == "__main__":
     main()
