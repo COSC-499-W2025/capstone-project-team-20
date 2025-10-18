cbc1f7b5f9b098582949bc4c70388e58b3b0d008
branden kennedy <brandenk88@gmail.com>
2025-10-18 16:11:37 -0700

    Restored gitignore and removed comments from main, just left the one about commenting for manual testing

--- src/main.py
+++ src/main.py
@@ -4,14 +4,13 @@ def main():
     consent = ConsentManager()
 
     # Uncomment two lines below to reset consent for testing manually
-    # from src.ConfigManager import ConfigManager
-    # ConfigManager().delete("user_consent") 
+    from src.ConfigManager import ConfigManager
+    ConfigManager().delete("user_consent") 
 
     if not consent.require_consent():
         print("Consent not given. Exiting program.")
         return
-    # if answer is yes, consent persists
-    # if answer is no, will re ask for consent each time
+    
     print("Consent confirmed in main.")
 
     
