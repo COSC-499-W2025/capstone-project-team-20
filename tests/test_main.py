8d2bef11e144fd687a9013bad68e5088010c2953
mr-sban <30375170+mr-sban@users.noreply.github.com>
2025-10-19 19:08:22 -0700

    fix: Properly mocks user consent 🐛

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
 from src.main import main
 
-def test_main_runs():
+def test_main_runs(monkeypatch):
     try:
+        # Mock user input as if someone pressed "y"
+        monkeypatch.setattr("builtins.input", lambda _: "y")
         main()
         assert True
     except Exception as e:
