fc93b2445f6fb03cdb80556b117892e1981574b1
dylanstephenalexander <dylanstephenalexander@gmail.com>
2025-10-20 15:50:51 -0700

    changed my approach in refactoring storage manager

--- src/ConfigManager.py
+++ src/ConfigManager.py
@@ -1,37 +1,37 @@
 from src.StorageManager import StorageManager
+from typing import Any
+from typing import Dict
 
 class ConfigManager(StorageManager):
 
     def __init__(self, db_path="config.db") -> None:
         super().__init__(db_path)
 
-    _create_table_query = """ CREATE TABLE IF NOT EXISTS configs (key TEXT PRIMARY KEY,value TEXT NOT NULL)"""
-    _set_value_query = """INSERT OR REPLACE INTO configs (key, value) VALUES (?, ?)"""
-    _get_value_query = """SELECT value FROM configs WHERE key = ?"""
-    _get_all_query = """SELECT key, value FROM configs"""
-    _delete_value_query = """DELETE FROM configs WHERE key = ?"""
-    _clear_query = """DELETE FROM configs"""
-    
     @property
     def create_table_query(self) -> str:
-        return self._create_table_query
+        return """ CREATE TABLE IF NOT EXISTS configs (key TEXT PRIMARY KEY,value TEXT)"""
+
+    @property
+    def table_name(self) -> str:
+        return "configs"
     
     @property
-    def set_value_query(self) -> str:
-        return self._set_value_query
+    def primary_key(self) -> str:
+        return "key"
     
     @property
-    def get_value_query(self) -> str:
-        return self._get_value_query
+    def columns(self) -> str:
+        return "key, value"
     
-    @property
-    def get_all_query(self) -> str:
-        return self._get_all_query
+    def set(self, key: str, value: Any) -> None:
+        super().set({"key": key, "value": value})
     
-    @property
-    def delete_value_query(self) -> str:
-        return self._delete_value_query
+    def get(self, key: str, default: Any = None) -> Any:
+        result = super().get(key, default)
+        if result and isinstance(result, dict):
+            return result.get("value", default)
+        return default
     
-    @property
-    def clear_query(self) -> str:
-        return self._clear_query
+    def get_all(self) -> Dict[str, Any]:
+        rows = super().get_all()
+        return {row["key"]: row["value"] for row in rows}
