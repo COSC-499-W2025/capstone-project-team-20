9c325f208d2faa340aefce6a43683bb5d2913f01
branden kennedy <brandenk88@gmail.com>
2025-10-28 15:49:30 -0700

    Finished the file categorizer. Edited ProjectMetadataExtractor so that it uses FileCategorizer. edited main because there was redundant code (asking for consent twice...)

--- src/analyzers/FileCategorizer.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pathlib import Path
-from collections import Counter
-from typing import List, Dict, Any
-
-from language_detector import LANGUAGE_MAP, MARKUP_LANGUAGE_MAP
-
-CONFIG_DIR = Path(__file__).parent.parent / "config"
-CATEGORIES_FILE = CONFIG_DIR / "categories.yml"
--- src/main.py
+++ src/main.py
@@ -14,22 +14,18 @@ def main():
     if not consent.require_consent():
         print("Consent not given. Exiting program.")
         return
-
-    zip_path = input("Enter the path to the zipped project file: ").strip()
-    # Enter file path, need full path. Example from my testing: /Users/admin/Desktop/3rdyear.zip
     
     print("Consent confirmed. The application will now proceed.")
 
-    while True:
-        zip_to_analyze = input("Please enter the path to the zip file you want to analyze: ")
-        if os.path.exists(zip_to_analyze) and zip_to_analyze.endswith('.zip'):
-            break
-        print("The provided path is invalid or the file is not a .zip file. Please try again.")
-
+    zip_path = input("Enter the path to the zipped project file: ").strip()
+    while not (os.path.exists(zip_path) and zip_path.endswith(".zip")):
+        zip_path = input("Invalid path or not a .zip file. try again: ").strip()
+    # Enter file path, need full path. Example from my testing: /Users/admin/Desktop/3rdyear.zip
+    
     analyzer = GitRepoAnalyzer()
-    analyzer.analyze_zip(zip_to_analyze)
+    analyzer.analyze_zip(zip_path)
 
-    print("\nProgram finished.")
+    print("\nGit analysis complete.")
 
 
     print(f"\nparsing project from: {zip_path}")
